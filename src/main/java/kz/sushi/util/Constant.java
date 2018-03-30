44702173d0564fd7137df6e9ee4188e8af709ddf
Arman1710 <33391860+Arman1710@users.noreply.github.com>
2018-03-30 18:05:29 +0600

    new commit

---
+++ src/main/java/kz/sushi/util/Constant.java
@@ -0,0 +1,40 @@
+package kz.sushi.util;
+
+public class Constant {
+
+//    attributes & parameters
+    public static final String SUCCESS = "success";
+    public static final String PRODUCT_LIST = "productList";
+    public static final String ORDERS_LIST = "ordersList";
+    public static final String TOTAL_COST = "totalCost";
+    public static final String LOCALE = "locale";
+    public static final String USER_ID = "userId";
+    public static final String PRODUCT_TYPE = "productType";
+    public static final String PRODUCT_ID = "prodId";
+    public static final String CURRENT_PAGE = "currentPage";
+    public static final String EMAIL = "email";
+    public static final String LOGIN = "login";
+    public static final String PASSWORD = "password";
+    public static final String PHONE = "phone";
+    public static final String ADDRESS = "address";
+    public static final String BIRTHDAY = "birthday";
+    public static final String ERROR_MESSAGE = "errorMsg";
+    public static final String LOGIN_ERROR = "loginError";
+
+//    page
+    public static final String BASKET_PAGE = "/WEB-INF/basket.jsp";
+    public static final String REGISTRATION_PAGE = "/WEB-INF/registration.jsp";
+    public static final String ADMIN_CABINET_PAGE = "/WEB-INF/admin-cabinet.jsp";
+    public static final String LOGIN_PAGE = "/WEB-INF/login.jsp";
+    public static final String INDEX_PAGE = "/WEB-INF/index.jsp";
+    public static final String USER_INDEX_PAGE = "/WEB-INF/user-index.jsp";
+    public static final String ROLLS_PAGE = "/WEB-INF/rolls.jsp";
+    public static final String USER_ROLLS_PAGE = "/WEB-INF/user-rolls.jsp";
+    public static final String SETS_PAGE = "/WEB-INF/sets.jsp";
+    public static final String USER_SETS_PAGE = "/WEB-INF/user-sets.jsp";
+
+//    other
+    public static final String BASKET_SUCCESS = "basket.success";
+    public static final int ADMIN_ROLE = 1;
+    public static final int USER_ROLE = 2;
+}
